b1b72fc095f60e86c8b14173fd1cd4a5a1a076a3
Atílio <atiliodadalto@protonmail.com>
2024-02-17 16:25:47 -0300

    refactor: actually use configs (add_soap_items)

--- Auto Use Soap/Mods/AutoUseSoap/ScriptExtender/Lua/Server/SubscribedEvents.lua
+++ Auto Use Soap/Mods/AutoUseSoap/ScriptExtender/Lua/Server/SubscribedEvents.lua
@@ -12,7 +12,7 @@ local function SubscribeToEvents()
       if JsonConfig.FEATURES.use_when_entering_camp == true then
         Utils.DebugPrint(1, "Teleported to camp. Checking for soap to use for " .. character)
 
-        if Utils.GetGUID(character) == Osi.GetHostCharacter() then
+        if Utils.GetGUID(character) == Osi.GetHostCharacter() and JsonConfig.FEATURES.add_soap_items == true then
           DeliverSoapToParty()
         end
 
