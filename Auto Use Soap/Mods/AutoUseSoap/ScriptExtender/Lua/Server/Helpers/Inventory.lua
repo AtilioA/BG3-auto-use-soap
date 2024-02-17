8639968368c750cfe0b2430a6968341cddae8ed9
Atílio <atiliodadalto@protonmail.com>
2024-02-17 16:06:06 -0300

    chore: remove unused code

--- Auto Use Soap/Mods/AutoUseSoap/ScriptExtender/Lua/Server/Helpers/Inventory.lua
+++ Auto Use Soap/Mods/AutoUseSoap/ScriptExtender/Lua/Server/Helpers/Inventory.lua
@@ -1,7 +1,5 @@
 -- Adapted from FocusBG3: https://www.nexusmods.com/baldursgate3/mods/5972
 
-local CAMP_SUPPLY_SACK_TEMPLATE_ID = 'efcb70b7-868b-4214-968a-e23f6ad586bc'
-
 ---@param object any
 ---@return EntityHandle|nil
 function GetHolder(object)
@@ -162,45 +160,4 @@ function GetInventory(object, primaryOnly, shallow)
 
   table.sort(items, function(a, b) return a.Name < b.Name end)
   return items
-end
-
-function GetCampChestInventory()
-  local campChest = Osi.DB_Camp_UserCampChest:Get(nil, nil)[1][2]
-  return GetInventory(campChest, true, true)
-end
-
---- Checks if an inventory contains a supply sack.
----@param inventoryItems any The first
----@return any | nil - The first supply sack object, or nil if not found.
-function TryToGetCampChestSupplyPack(inventoryItems)
-  for _, item in ipairs(inventoryItems) do
-    if item.TemplateId == CAMP_SUPPLY_SACK_TEMPLATE_ID then
-      return item
-    end
-  end
-
-  return nil
-end
-
-function CheckForCampChestSupplySack()
-  return TryToGetCampChestSupplyPack(GetCampChestInventory())
-end
-
-function AddSupplySackToCampChest()
-  local campChest = Osi.DB_Camp_UserCampChest:Get(nil, nil)[1][2]
-  Utils.DebugPrint(1, "Adding supply sack to camp chest: " .. campChest)
-  Osi.TemplateAddTo(CAMP_SUPPLY_SACK_TEMPLATE_ID, campChest, 1)
-end
-
-function AddSupplySackToCampChestIfMissing()
-  if CheckForCampChestSupplySack() ~= nil then
-    Utils.DebugPrint(3, "Supply sack found in camp chest.")
-  else
-    Utils.DebugPrint(3, "Supply sack not found in camp chest. Creating one.")
-    AddSupplySackToCampChest()
-  end
-end
-
-function GetCampChestSupplySack()
-  return CheckForCampChestSupplySack()
 end
